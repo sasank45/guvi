fddd953e3e1b22b1edcd2e237c54cd30a5e635e2
sasank <35967826+sasank45@users.noreply.github.com>
2019-06-11 14:54:23 +0530

    Update even.c

--- even.c
+++ even.c
@@ -2,10 +2,13 @@
 void main(){
   int n;
   scanf("%d",&n);
-  if(n%2==0)
-    printf("Even");
-  else if(n != 0)
-    printf("Odd");
+  if(n>0)
+  {
+    if(n%2==0)
+      printf("Even");
+    else
+      printf("Odd");
+  }
   else
     printf("invalid");
 }  
