a139d25c680ba1fae849e8c73f32ccb441950f51
sasank <35967826+sasank45@users.noreply.github.com>
2019-07-02 23:01:34 +0530

    Update count_digits.c

--- count_digits.c
+++ count_digits.c
@@ -3,6 +3,7 @@ int main()
 {
     int n,i,count=0,rem;
     scanf("%d",&n);
+    if(n>0){
     while(rem!=0)
     {
         rem = n/10;
@@ -10,5 +11,8 @@ int main()
         count++;
     }
     printf("%d",count);
+    }
+    else
+        printf("Not possible");
     return 0;
 }   
