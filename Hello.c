332bcdc68d8615bbd39e487ba040580fb4b9c835
sasank <35967826+sasank45@users.noreply.github.com>
2019-07-05 22:28:02 +0530

    Update Hello.c

--- Hello.c
+++ Hello.c
@@ -2,8 +2,13 @@
 int main(){
     int n,i;
     scanf("%d",&n);
+    if(n>=0){
     for(i=1;i<=n;i++){
         printf("Hello\n");
+    }
+    }
+    else{
+        printf("No");
     }
     return 0;
 }
