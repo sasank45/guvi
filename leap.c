cc590ae872734bdbf91d10231dd34d4e69ca1da9
sasank <35967826+sasank45@users.noreply.github.com>
2019-07-08 23:39:27 +0530

    Update leap.c

--- leap.c
+++ leap.c
@@ -3,7 +3,6 @@ int main()
 {
     int n;
     scanf("%d",&n);
-    if(n>0){
     if(n%400==0){
         if(n%100==0){
             if(n%4==0){
@@ -13,7 +12,6 @@ int main()
     }
     else{
         printf("no");
-    }
     }
     return 0;
 }
