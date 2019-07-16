86082382a7a8b97dac3e416533a2bf0f523132d3
sasank <35967826+sasank45@users.noreply.github.com>
2019-07-16 11:16:51 +0530

    Update rangeodd.py

--- rangeodd.py
+++ rangeodd.py
@@ -1,7 +1,5 @@
 n,k = map(int,input().split())
-if k-n>1:
-    for i in range(n+1,k):
-        if(i%2 != 0):
-            print(i)
-else:
-    print("no numbers")
+for i in range(n+1,k):
+    if(i%2 != 0):
+        print(i,end=" ")
+
