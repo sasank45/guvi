2dd916bcdb29441f1d3e14e9dfa29df7afe624ad
sasank <35967826+sasank45@users.noreply.github.com>
2019-07-16 14:30:36 +0530

    Update hrmin.py

--- hrmin.py
+++ hrmin.py
@@ -1,4 +1,6 @@
 n1 = list(map(int,input().split()))
 n2 = list(map(int,input().split()))
-print(n2[0]-n1[0],end=" ")
-print(n2[1]-n1[1])
+y1 = n2[0]-n1[0]
+y2 = n2[1]-n1[1]
+print(abs(y1),end=" ")
+print(abs(y2))
