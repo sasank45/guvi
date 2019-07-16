b09a64bc75d9463fc9e8682eeef808376ae72e6c
sasank <35967826+sasank45@users.noreply.github.com>
2019-07-16 13:22:01 +0530

    Create range_armstrong.py

---
+++ range_armstrong.py
@@ -0,0 +1,10 @@
+n1,n2 = map(int,input().split())
+for i in range(n1,n2+1):
+    temp = i
+    s = 0
+    while temp > 0:
+        digit = temp % 10
+        s += digit ** 3
+        temp //= 10
+    if i == s:
+        print(i)
